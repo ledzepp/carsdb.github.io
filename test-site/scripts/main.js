bd9badeb91daa579e1e2bc04c1eb25daaa6149a1
Prateek Sharma <prateeksharmaeng@gmail.com>
2021-05-22 12:27:34 +0530

    initial commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,39 @@
+let myImage = document.querySelector('img');
+
+myImage.onmouseover = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/website-image.jpg') {
+        myImage.setAttribute('src', 'images/website-image-2.jpg')
+    }
+}
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/website-image-2.jpg') {
+        myImage.setAttribute('src', 'images/website-image.jpg')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name: ');
+    if(!myName){
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Cars are cool! ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Cars are cool! ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
